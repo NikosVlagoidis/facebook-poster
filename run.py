aefe4697a26113136f6c1290b53c04f8daa624a1
NikosVlagoidis <vlagoidis@vermantia.com>
2017-02-22 18:05:28 +0200

    Command line functionality

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
+#!/usr/bin/python
+
 import facebook
 from decouple import config
+import sys
 
 FACEBOOK_TOKEN = config('FACEBOOK_TOKEN')
 USER_ID = config('USER_ID')
-
 facebook_graph = facebook.GraphAPI(FACEBOOK_TOKEN)
+msg = sys.argv
+
 
 try:
-    fb_response = facebook_graph.put_wall_post('Your Wall Post', profile_id=USER_ID)
+    fb_response = facebook_graph.put_wall_post(msg, profile_id=USER_ID)
     print fb_response
 except facebook.GraphAPIError as e:
     print 'Something went wrong:', e.type, e.message
